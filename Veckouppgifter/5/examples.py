139876b3edc3cd609680499759b0aa9f34e437bd
henrikcederqvist <cederqvist.henrik@gmail.com>
2026-02-26 20:48:33 +0100

    Update

--- Veckouppgifter/5/examples.py
+++ Veckouppgifter/5/examples.py
@@ -1,2 +1,9 @@
+from math import sqrt
+
+
 def add(x, y):
     return x + y
+
+# en funktion som kan räkna ut hypotenusan med Pythagoras sats
+def hypo(a, b):
+    return sqrt(a * a + b * b)
--- Veckouppgifter/5/tests/test_examples.py
+++ Veckouppgifter/5/tests/test_examples.py
@@ -1,6 +1,19 @@
-from examples import add
+from examples import add, hypo
+from pytest import approx
 
-def test_add():
+def test_add_integers():
     expected = 3
     actual = add(1, 2)
+    assert actual == expected
+
+def test_add_floats():
+    expected = 0.3
+    actual = add(0.1, 0.2)
+    assert approx(actual) == expected
+
+def test_hypo():
+    #testdata: a=5, b=12, c=13
+    expected = 13
+    actual = hypo(5, 12)
+    # obs - hypo returnerar en float
     assert actual == expected
